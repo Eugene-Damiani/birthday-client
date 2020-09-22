4e2d0f2c87e035ec5a075401e3edf203e7c8022e
Eugene-Damiani <damianieugene@gmail.com>
2020-09-22 03:50:21 -0700

    added new design to app

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Layout = props => (
   <div>
     <h1>Birthday Reminder</h1>
-    <h5>Never Be that Friend Again</h5>
+    <h5>Never Be That Person Again</h5>
 
     {props.children}
 
