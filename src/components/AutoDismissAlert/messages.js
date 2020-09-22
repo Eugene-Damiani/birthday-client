6f3ce1aa1474d347bce8b4485e5df0892603dcc4
Eugene-Damiani <damianieugene@gmail.com>
2020-09-21 17:37:00 -0700

    messages added

--- src/components/AutoDismissAlert/messages.js
+++ src/components/AutoDismissAlert/messages.js
@@ -5,5 +5,15 @@ export default {
   signInFailure: 'Failed to sign in. Check your email and password and try again.',
   signOutSuccess: 'Come back soon!',
   changePasswordSuccess: 'Password changed successfully!',
-  changePasswordFailure: 'Failed to change passwords. Check your old password and try again.'
+  changePasswordFailure: 'Failed to change passwords. Check your old password and try again.',
+  indexSuccess: 'Look at your gift list You Generous Beast!',
+  indexFailure: 'Your list didn\'t load try again',
+  listShowSuccess: 'Go Get That Gift',
+  listShowFailure: 'Please Try Again',
+  giftDeletedSuccess: 'It Is Gone Gone Gone',
+  giftDeletedFailure: 'Your Gift Is Still On Your List',
+  wishListCreateSuccess: 'Gift Away',
+  wishListCreateFailure: 'Nope.com',
+  editSuccess: 'Out With The Old and In With The New!',
+  editFailure: 'Still Old'
 }
